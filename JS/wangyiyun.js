d1d94b0c98fc40fbcd274e7ddd87ef36d6869eb1
Vip4pt <vip4pt@gmail.com>
2025-08-27 18:37:19 +0800

    Create wangyiyun.js

---
+++ JS/wangyiyun.js
@@ -0,0 +1,119 @@
+/*
+ *
+ *
+脚本功能：网易云音乐 
+软件版本：9.3.60~最新
+下载地址：
+脚本作者：
+更新时间：2025
+问题反馈：
+使用声明：此脚本仅供学习与交流，请在下载使用24小时内删除！请勿在中国大陆转载与贩卖！
+*******************************
+[rewrite_local]
+# > 网易云音乐
+# 播放器会员皮肤
+^https:\/\/interface3?\.music\.163\.com\/eapi\/playermode\/ url script-request-header https://raw.githubusercontent.com/Vip4pt/Quantumult-X-JS/refs/heads/main/JS/wangyiyun.JS
+# 搜索结果会员歌曲
+^https:\/\/interface3?\.music\.163\.com\/eapi\/search\/complex\/(page|rec\/song\/get) url script-request-header https://raw.githubusercontent.com/Vip4pt/Quantumult-X-JS/refs/heads/main/JS/wangyiyun.JS
+# 播放器会员歌曲
+^https:\/\/interface3?\.music\.163\.com\/eapi\/v3\/song\/detail url script-request-header https://raw.githubusercontent.com/Vip4pt/Quantumult-X-JS/refs/heads/main/JS/wangyiyun.JS
+^https:\/\/interface3?\.music\.163\.com\/eapi\/song\/(chorus|enhance\/|play\/|type\/detail\/get) url script-request-header https://raw.githubusercontent.com/Vip4pt/Quantumult-X-JS/refs/heads/main/JS/wangyiyun.JS
+^https:\/\/interface3?\.music\.163\.com\/eapi\/(v1\/artist\/top\/song|v3\/discovery\/recommend\/songs) url script-request-header https://raw.githubusercontent.com/Vip4pt/Quantumult-X-JS/refs/heads/main/JS/wangyiyun.JS
+# 侧边栏会员等级
+^https:\/\/interface3?\.music\.163\.com\/eapi\/vipnewcenter\/app\/resource\/newaccountpage url script-request-header https://raw.githubusercontent.com/Vip4pt/Quantumult-X-JS/refs/heads/main/JS/wangyiyun.JS
+# 首页歌单会员歌曲
+^https?:\/\/interface3?\.music\.163\.com\/eapi\/(homepage\/|v6\/)?playlist\/ url script-request-header https://raw.githubusercontent.com/Vip4pt/Quantumult-X-JS/refs/heads/main/JS/wangyiyun.JS
+# 会员认证
+^https?:\/\/interface3?\.music\.163\.com\/eapi\/vipauth\/app\/auth\/(soundquality\/)?query url script-request-header https://raw.githubusercontent.com/Vip4pt/Quantumult-X-JS/refs/heads/main/JS/wangyiyun.JS
+# 去除部分广告
+^https?://interface.*\.music\.163\.com/eapi/ad/get url reject
+^https?://interface.*\.music\.163\.com/eapi/ad/config/get url reject
+^https?://interface.*\.music\.163\.com/eapi/ad/iyunIds url reject
+^https?://interface.*\.music\.163\.com/eapi/ad/prefetch/select url reject
+^https?://interface.*\.music\.163\.com/eapi/ad/loading/current url reject
+[mitm] 
+hostname = *music.163.com,interface*.music.163.com,
+
+*
+*
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+//Wed Aug 27 2025 10:32:26 GMT+0000 (Coordinated Universal Time)
+//Base:https://github.com/echo094/decode-js
+//Modify:https://github.com/smallfawn/decode_action
+var encode_version = "jsjiami.com.v5";
+var _0x6cbacf = $request.headers;
+_0x6cbacf.cookie = "MUSIC_U=00C9610D4E969926FA2513F7B324A9031F128BDA1073A2AEE61F37F42A038B511A2537B2D15757F9D3D53098AD04266F63B29BE102A4A1268DCA3EBC667570EA093E09CCA5C132472FABF1BC5E1B3C15828448B580EBC82E2C503C96590C5840C2212777D84204C7C2569EFFE16D738BC6C413A2F383F3098E31A4A0E3BC19A9FBA217D7BBB0CB3A09050DE13334AB04B5DCB651F19E9CAF67C6F9A25B5DC0258A403B11DED5E4C469ACC0968847CC20F9BE7902837FF2884D8BD741F2F03E28E2B8B37C72E02A5355F5017FC95937EA3B443292A631037C2C20B9C2C4D9BDD1F1E39B603B497D20917F854A95634BEC73BA6DD09AC255CA31CFE41820F6B1E22E9F2228EF7003A7DF0FDC626A4C12E14F4D594BF4CBCD2FF84591E5B87FC50DBDF4B125A1E8F61651ACEBC74C649CF55A8C31B9E61A7BDEDB4AFFBFDE149C9A52CF00A0A9DF5DFB30811942563FDD2147114831657C4D698EED3E8B504F7A4B8D;appver=9.3.25; deviceId=5664b3c08da1d786701cc1a79279c035; os=iPhone OS; osver=16.2; machineid=iPhone13.4; ";
+_0x6cbacf["user-agent"] = "NeteaseMusic 9.3.25/6062 (iPhone; iOS 16.6; zh-Hans_TW)";
+_0x6cbacf["mconfig-Info"] = "{\"zr4bw6pKFDIZScpo\":{\"version\":3061824,\"appver\":\"9.3.25\"},\"tPJJnts2H31BZXmp\":{\"version\":3973120,\"appver\":\"2.0.30\"},\"c0Ve6C0uNl2Am0Rl\":{\"version\":600064,\"appver\":\"1.7.50\"},\"IuRPVVmc3WWul9fT\":{\"version\":92171072,\"appver\":\"9.3.25\"}}";
+$done({
+  headers: _0x6cbacf
+});
+(function (_0x6e2f39, _0x54a727, _0x334f95) {
+  var _0x56818f = function () {
+    var _0x4341af = true;
+    return function (_0x426550, _0x441b1f) {
+      var _0x58fc72 = _0x4341af ? function () {
+        if (_0x441b1f) {
+          var _0x54e54f = _0x441b1f.apply(_0x426550, arguments);
+          _0x441b1f = null;
+          return _0x54e54f;
+        }
+      } : function () {};
+      _0x4341af = false;
+      return _0x58fc72;
+    };
+  }();
+  var _0x2bd5ab = _0x56818f(this, function () {
+    var _0x230f55 = function () {};
+    var _0x41c4b7 = typeof window !== "undefined" ? window : typeof process === "object" && typeof require === "function" && typeof global === "object" ? global : this;
+    if (!_0x41c4b7.console) {
+      _0x41c4b7.console = function (_0x2fdd4e) {
+        var _0x334f95 = {};
+        _0x334f95.log = _0x2fdd4e;
+        _0x334f95.warn = _0x2fdd4e;
+        _0x334f95.debug = _0x2fdd4e;
+        _0x334f95.info = _0x2fdd4e;
+        _0x334f95.error = _0x2fdd4e;
+        _0x334f95.exception = _0x2fdd4e;
+        _0x334f95.trace = _0x2fdd4e;
+        return _0x334f95;
+      }(_0x230f55);
+    } else {
+      _0x41c4b7.console.log = _0x230f55;
+      _0x41c4b7.console.warn = _0x230f55;
+      _0x41c4b7.console.debug = _0x230f55;
+      _0x41c4b7.console.info = _0x230f55;
+      _0x41c4b7.console.error = _0x230f55;
+      _0x41c4b7.console.exception = _0x230f55;
+      _0x41c4b7.console.trace = _0x230f55;
+    }
+  });
+  _0x2bd5ab();
+  _0x334f95 = "al";
+  try {
+    _0x334f95 += "ert";
+    _0x54a727 = encode_version;
+    if (!(typeof _0x54a727 !== "undefined" && _0x54a727 === "jsjiami.com.v5")) {
+      _0x6e2f39[_0x334f95]("删除版本号，js会定期弹窗，还请支持我们的工作");
+    }
+  } catch (_0x2d7ea7) {
+    _0x6e2f39[_0x334f95]("删除版本号，js会定期弹窗");
+  }
+})(window);
+encode_version = "jsjiami.com.v5";
